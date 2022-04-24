0e101da6c163e14eb5ae7f33430b0411abfdbc4f
junaid khurshid <96163312+junaid71@users.noreply.github.com>
2022-04-24 22:57:52 +0530

    Add files via upload

---
+++ virtual functions.cpp
@@ -0,0 +1,34 @@
+//virtual functions
+//junaid khursheed
+#include<iostream>
+using namespace std;
+
+class Base
+{
+    public:
+	virtual void fun()
+	{
+		cout<<"fun of Base"<<endl;
+	}
+	
+};
+    
+class Derived:public Base
+{
+    public:
+	void fun()
+	{
+		cout<<"fun of Derived"<<endl;
+	}
+	
+};
+    
+int main()
+{
+	Derived d;
+	d.fun();
+	Base *ptr=&d;
+	ptr->fun();
+	    
+}
+    
